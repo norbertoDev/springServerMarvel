299e6a3174b77cf10108e5eb174a387a05bf4566
Norberto <alu0100611519@ull.edu.es>
2022-03-01 00:13:22 +0100

    first commit

---
+++ src/main/java/com/marvel/marvelServer/MarvelServerApplication.java
@@ -0,0 +1,13 @@
+package com.marvel.marvelServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarvelServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarvelServerApplication.class, args);
+	}
+
+}
